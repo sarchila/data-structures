b49759562766f2b7408a767f27c05cc5fcdaaa42
Santiago <santiago.archila@gmail.com>
2013-11-13 13:25:05 -0800

    functional stacks implemented

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,17 +3,24 @@ var makeStack = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
-  var size; // Hint: set an initial value here
+  var size = 0; // Hint: set an initial value here
 
   // Implement the methods below
 
   instance.push = function(value){
+    instance[instance.size()] = value;
   };
 
   instance.pop = function(){
+    val = instance[instance.size()-1];
+    delete instance[instance.size()-1];
+    return val;
   };
 
   instance.size = function(){
+    // minus three because I'm subtracting the 3
+    // built-in functions.
+    return Object.keys(instance).length - 3;
   };
 
   return instance;
