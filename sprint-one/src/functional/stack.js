c487719fe271a72a6b01b0761551e583a769ccea
Hendrixer <willscottmoss@gmail.com>
2013-11-13 13:30:16 -0800

    first commit

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,17 +3,21 @@ var makeStack = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
-  var size; // Hint: set an initial value here
+  var size = 0; // Hint: set an initial value here
 
   // Implement the methods below
 
   instance.push = function(value){
+    size++;
   };
 
   instance.pop = function(){
+    //debugger;
+    size && size--;
   };
 
   instance.size = function(){
+    return size;
   };
 
   return instance;
