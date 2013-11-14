12e75137dc31b083cd299479c65d0f34c0d29f40
Hack Reactor Students <santiago.archila@gmail.com>
2013-11-14 10:26:19 -0800

    prototypal stack implemented

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,3 +1,20 @@
 var Stack = function() {
-  // Hey! Copy your code from src/prototypal/stack.js and paste it here
+  this.storage = {};
+  this.length = 0;
+};
+
+Stack.prototype.push = function(value){
+    this.storage[this.length] = value;
+    this.length++;
+  };
+
+Stack.prototype.pop = function(){
+    var val = this.storage[this.length - 1];
+    delete this.storage[this.length - 1];
+    this.length && this.length--;
+    return val;
+  };
+
+Stack.prototype.size = function(){
+    return this.length;
 };
