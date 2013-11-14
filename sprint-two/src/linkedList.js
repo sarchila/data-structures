5f4cf8470b8abf0654bba6581c9253b56f2d60c3
Hack Reactor Students <santiago.archila@gmail.com>
2013-11-14 14:25:51 -0800

    address initial tests

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,14 +3,26 @@ var makeLinkedList = function(){
   var list = {};
   list.head = null;
   list.tail = null;
+  var prevVal = null;
 
-  list.addToTail = function(){
+  list.addToTail = function(value){
+    //add value to end of list
+    list[value] = makeNode(value);
+    // list[prevVal].next = list[value];
+    list.tail = list[value];
+    if (list.head === null){
+      list.head = list[value];
+    }
+    prevVal = value;
+    // list[prevVal] = list[currVal];
   };
 
   list.removeHead = function(){
+    //removes node from head and returns it
   };
 
   list.contains = function(){
+    //check if node is in tree, boolean
   };
 
   return list;
