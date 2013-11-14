177871bc4f9852262e5e41ac2743e3770c9b386a
Santiago <santiago.archila@gmail.com>
2013-11-14 14:10:20 -0800

    complete merge with pair's remote

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,23 +1,10 @@
 var makeStack = function() {
-<<<<<<< HEAD
   // Hey! Copy your code from src/functional/stack.js and paste it here
   var instance = Object.create(stackMethods);
 
   // Use an object with numeric keys to store values
   instance.storage = {};
   instance.length = 0; // Hint: set an initial value here
-=======
-  var instance = {};
-
-  instance.storage = {};
-  instance.length = 0; 
-
-  instance.push = stackMethods.push;
-
-  instance.pop = stackMethods.pop;
-
-  instance.size = stackMethods.size;
->>>>>>> 0733034f017794670aca24fedf06f3a6ca736f9d
 
   return instance;
 };
@@ -36,8 +23,4 @@ var stackMethods = {
   size: function(){
     return this.length;
   }
-<<<<<<< HEAD
 };
-=======
-};
->>>>>>> 0733034f017794670aca24fedf06f3a6ca736f9d
