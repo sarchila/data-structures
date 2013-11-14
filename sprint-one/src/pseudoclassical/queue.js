5fa4ec7e0d981c72cccc0796ca6d51dc610382f0
Hack Reactor Students <santiago.archila@gmail.com>
2013-11-14 10:33:27 -0800

    queue psuedo all test green

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,3 +1,27 @@
 var Queue = function() {
-  // Hey! Copy your code from src/prototypal/queue.js and paste it here
+  this.storage = {};
+  this.length = 0;
+  this.index = 0;
+  this.dqCounter = 0;
+};
+
+
+Queue.prototype.enqueue = function(value){
+  this.storage[this.index] = value;
+  this.length++;
+  this.index++;
+};
+
+Queue.prototype.dequeue = function(){
+  var val = this.storage[this.dqCounter];
+  delete this.storage[this.dqCounter];
+  if (this.length > 0){
+    this.length--;
+    this.dqCounter++;
+  }
+  return val;
+};
+
+Queue.prototype.size = function(){
+  return this.length;
 };
