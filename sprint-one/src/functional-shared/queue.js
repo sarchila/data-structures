8a5ff31a2cd558f140df54192e74773756b6969a
Hack Reactor Students <santiago.archila@gmail.com>
2013-11-13 20:17:48 -0800

    functional shared queue passes all tests

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,5 +1,34 @@
 var makeQueue = function(){
-  // Hey! Copy your code from src/functional/queue.js and paste it here
+  var instance = {};
+
+  instance.storage = {};
+  instance.length = 0;
+  instance.index = 0;
+  instance.dqCounter = 0;
+
+  instance.enqueue = queueMethods.enqueue;
+  instance.dequeue = queueMethods.dequeue;
+  instance.size = queueMethods.size;
+
+  return instance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value){
+    this.storage[this.index] = value;
+    this.length++;
+    this.index++;
+  },
+  dequeue: function(){
+    var val = this.storage[this.dqCounter];
+    delete this.storage[this.dqCounter];
+    if (this.length > 0){
+      this.length--;
+      this.dqCounter++;
+    }
+    return val;
+  },
+  size: function(){
+    return this.length;
+  }
+};
