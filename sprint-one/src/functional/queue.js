e18cdd0de006b0c37db8780323b727400b80646c
Santiago <santiago.archila@gmail.com>
2013-11-13 20:44:50 -0800

    queue functional pattern passes all tests

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,17 +3,30 @@ var makeQueue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var length = 0;
+  var index = 0;
+  var dqCounter = 0;
 
   // Implement the methods below
 
   instance.enqueue = function(value){
+    storage[index] = value;
+    length++;
+    index++;
   };
 
   instance.dequeue = function(){
+    var val = storage[dqCounter];
+    delete storage[dqCounter];
+    if (length > 0){
+      length--;
+      dqCounter++;
+    }
+    return val;
   };
 
   instance.size = function(){
+    return length;
   };
-
   return instance;
 };
