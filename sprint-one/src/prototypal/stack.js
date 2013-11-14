2200a3c956539fc6598b2dd9df4e6d8c44dc5eff
Santiago <santiago.archila@gmail.com>
2013-11-13 23:32:13 -0800

    copy from functional-shared

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,5 +1,30 @@
 var makeStack = function() {
-  // Hey! Copy your code from src/functional-shared/stack.js and paste it here
+  var instance = {};
+
+  instance.storage = {};
+  instance.length = 0; 
+
+  instance.push = stackMethods.push;
+
+  instance.pop = stackMethods.pop;
+
+  instance.size = stackMethods.size;
+
+  return instance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value){
+    this.storage[this.length] = value;
+    this.length++;
+  },
+  pop: function(){
+    var val = this.storage[this.length - 1];
+    delete this.storage[this.length - 1];
+    this.length && this.length--;
+    return val;
+  },
+  size: function(){
+    return this.length;
+  }
+};
