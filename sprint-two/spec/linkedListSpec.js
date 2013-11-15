1adcb2d5ebf08ecbde31655c14383600436c687e
Hack Reactor Students <santiago.archila@gmail.com>
2013-11-14 18:42:43 -0800

    changed spec to check nodes

--- sprint-two/spec/linkedListSpec.js
+++ sprint-two/spec/linkedListSpec.js
@@ -19,15 +19,15 @@ describe("linkedList", function() {
   // add more tests here to test the functionality of linkedList
   it("should return head and tail after adding to tail only once", function() {
     linkedList.addToTail(23);
-    expect(linkedList.tail).toEqual(linkedList['23']);
-    expect(linkedList.head).toEqual(linkedList['23']);
+    expect(linkedList.tail).toEqual({value:23, next: null});
+    expect(linkedList.head).toEqual({value:23, next: null});
   });
 
   it("should return head with first value after adding a second value to tail", function() {
     linkedList.addToTail(23);
     linkedList.addToTail(50);
-    expect(linkedList.tail).toEqual(linkedList['50']);
-    expect(linkedList.head).toEqual(linkedList['23']);
+    expect(linkedList.tail).toEqual({value:50, next: null});
+    expect(linkedList.head).toEqual({value:23,next: {value:50, next: null}});
   });
 
   it("should update '.next' property of head after adding second value to tail", function(){
