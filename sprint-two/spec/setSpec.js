92d1457ed5f9cb12368783d7a5b75795b0bddb06
Hack Reactor Students <santiago.archila@gmail.com>
2013-11-15 15:41:37 -0800

    wrote some set tests

--- sprint-two/spec/setSpec.js
+++ sprint-two/spec/setSpec.js
@@ -10,4 +10,15 @@ describe("set", function() {
     expect(set.contains).toEqual(jasmine.any(Function));
     expect(set.remove).toEqual(jasmine.any(Function));
   });
+
+  it("it should contain value that was added to it with '.add'", function() {
+    set.add('wassup');
+    expect(set.contains('wassup')).toEqual(true);
+  });
+  it("it should not contain value after being removed", function() {
+    set.add('yo');
+    set.add('me');
+    set.remove('yo');
+    expect(set.contains('yo')).toEqual(false);
+  });
 });
